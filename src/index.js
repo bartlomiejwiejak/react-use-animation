e8d2284f0b476f3b4889408024e9e5394a6bd535
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-11-07 17:50:13 +0100

    useAnimation hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-import React, { useState } from 'react'
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/no-duplicates */
+import { useEffect, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 
-export const ExampleComponent = ({ text }) => {
-  const [count, setCount] = useState(0)
-  return <div onClick={() => setCount((prev) => prev + 1)} >Example Component: {text} Count: {count}</div>
+import { Context } from './components/AnimationProvider'
+import AnimationProvider from './components/AnimationProvider'
+import Link from './components/Link'
+
+const useAnimation = (
+  enterAnimation,
+  leaveAnimation,
+  transition = 1000,
+  cleanup
+) => {
+  const { state, dispatch } = useContext(Context)
+  const history = useHistory()
+
+  useEffect(() => {
+    enterAnimation()
+    return cleanup
+  }, [])
+
+  useEffect(() => {
+    if (state.isAnimating) {
+      leaveAnimation()
+      setTimeout(() => {
+        dispatch({ type: 'ANIMATION_FINISH' })
+        history.push(state.path)
+      }, transition)
+    }
+  }, [state.isAnimating])
 }
+
+
+export default useAnimation
+export { AnimationProvider, Link }
